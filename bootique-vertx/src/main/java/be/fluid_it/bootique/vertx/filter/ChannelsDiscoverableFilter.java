8b472f414d3be99513e01792287833350d632431
ROEVENS Koen <koen.roevens@ext.ec.europa.eu>
2017-09-13 12:00:01 +0200

    Added unpublishing of autodiscoverable channels

--- bootique-vertx/src/main/java/be/fluid_it/bootique/vertx/filter/ChannelsDiscoverableFilter.java
+++ bootique-vertx/src/main/java/be/fluid_it/bootique/vertx/filter/ChannelsDiscoverableFilter.java
@@ -3,6 +3,8 @@ package be.fluid_it.bootique.vertx.filter;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.rxjava.ext.web.handler.sockjs.BridgeEvent;
 import io.vertx.rxjava.servicediscovery.types.MessageSource;
@@ -10,6 +12,8 @@ import io.vertx.servicediscovery.Record;
 import io.vertx.servicediscovery.ServiceDiscovery;
 
 public class ChannelsDiscoverableFilter implements Handler<BridgeEvent> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private final Handler<BridgeEvent> handler;
     private final Vertx vertx;
     private final String serviceName;
@@ -23,18 +27,39 @@ public class ChannelsDiscoverableFilter implements Handler<BridgeEvent> {
 
     @Override
     public void handle(BridgeEvent bridgeEvent) {
-        if (bridgeEvent.type() == BridgeEventType.REGISTER) {
-            ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
-            JsonObject message = bridgeEvent.getRawMessage();
-            String address = message.getString("address");
+        switch (bridgeEvent.type()) {
+            case REGISTER:
+                {
+                    ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+                    JsonObject message = bridgeEvent.getRawMessage();
+                    String address = message.getString("address");
 
-            Record record = MessageSource.createRecord(serviceName, address);
-            discovery.publish(record, ar -> {
-                if (ar.succeeded()) {
-                    System.out.printf("Successfully registered channel [%s, %s] ...\n", serviceName, address);
+                    Record record = MessageSource.createRecord(serviceName, address);
+                    discovery.publish(record, ar -> {
+                        if (ar.succeeded()) {
+                            System.out.printf("Successfully registered channel [%s, %s] ...\n", serviceName, address);
+                        }
+                    });
+                    discovery.close();
                 }
-                discovery.close();
-            });
+                break;
+            case SOCKET_CLOSED:
+            case UNREGISTER:
+                {
+                    ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
+                    JsonObject message = bridgeEvent.getRawMessage();
+                    String address = message.getString("address");
+
+                    Record record = MessageSource.createRecord(serviceName, address);
+                    discovery.unpublish(record.getRegistration(), ar -> {
+                        if (ar.succeeded()) {
+                            logger.info("Successfully unregistered channel [%s, %s] ...\n", serviceName, address);
+                        }
+                    });
+                    discovery.close();
+                }
+                break;
+            default:
         }
         handler.handle(bridgeEvent);
     }
